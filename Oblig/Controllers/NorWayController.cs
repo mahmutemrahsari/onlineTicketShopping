56d1ef50355bb0c701af1f818ff1c3c433c6ea59
U1_66 <13524215158@163.com>
2020-09-09 15:51:02 +0200

    Add project files.

---
+++ Oblig/Controllers/NorWayController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Mvc;
+using Oblig.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Oblig.Controllers
+{
+    [Route("[controller]/[action]")]
+    public class NorWayController : ControllerBase
+    {
+        private readonly BillettContext _db;
+
+        public NorWayController(BillettContext db)
+        {
+            _db = db;
+        }
+
+
+    }
+}
