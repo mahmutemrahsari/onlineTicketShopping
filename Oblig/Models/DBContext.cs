d62989235de368324a5de11b441f82510a598488
U1_66 <13524215158@163.com>
2020-09-09 15:51:02 +0200

    Add project files.

---
+++ Oblig/Models/DBContext.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Storage;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Oblig.Models
+{
+    
+        public class Kunde
+        {
+            public int Id { get; set; }
+            //public String Fornavn { get; set; }
+            //public String Etternavn { get; set; }
+            public String Epost { get; set; }
+            public String Telefonnr { get; set; }
+        }
+
+        public class Billett
+        {
+            [Key]
+            [System.ComponentModel.DataAnnotations.Schema.DatabaseGenerated(DatabaseGeneratedOption.None)]
+            public String Billettype { get; set; }
+            public int Pris { get; set; }
+            public String FraSted { get; set; }
+            public String AvgangersDato { get; set; }
+            public String TilSted { get; set; }
+            public String ReturDato { get; set; }
+        }
+
+        public class BillettContext : DbContext
+    {
+        public BillettContext(DbContextOptions<BillettContext> options) : base(options)
+        {
+            Database.EnsureCreated();
+        }
+    }
+    }
+
