92c7176ffc1da5005b85854835d58ae73d2cbfe1
Sasacoriror <43811709+Sasacoriror@users.noreply.github.com>
2020-09-10 15:29:55 +0200

    Js

---
+++ Oblig/wwwroot/Bestilling.js
@@ -0,0 +1,14 @@
+﻿function lagreBestilling() {
+    const reise = {
+        navn: $("#navn").val(),
+        telefonnr: $("#telfonnr").val(),
+        strekning: $("#strekning").val(),
+        avganger: $("#avganger").val(),
+        // noe: $("input:radio[name=noe]:checked").val(),
+        priser: $("#priser").val()
+    };
+    const url = /*Navn på klassen i models*//SettInn";
+    $.post(url, reise, function () {
+        window.location.href = "index.html";
+    });
+};
---
+++ Oblig/wwwroot/index.js
@@ -0,0 +1,24 @@
+﻿$(function () {
+    $.get("(fyll inn med navn på klassen i models)/HentAlle", function (bestillinger) {
+        formaterBestillinger(bestillinger);
+    });
+});
+
+function formaterBestillinger(bestillinger) {
+    let ut = "<table class='table table-striped'>" +
+        "<tr>" +
+        "<th>Navn</th><th>Telefonnr</th>Strekninger<th></th><th>Avganger</th>" +
+        "<th>Priser</th>" +
+        "</tr>";
+    for (let bestilling of bestillinger) {
+        ut += "<tr>" +
+            "<td>" + bestilling.navn + "</td>" +
+            "<td>" + bestilling.telefonnr + "</td>" +
+            "<td>" + bestilling.strekning + "</td>" +
+            "<td>" + bestilling.avganger + "</td>" +
+            "<td>" + bestilling.priser + "</td>" +
+            "</tr>";
+    }
+    ut += "</table>";
+    $("#bestillinger").html(ut);
+}
