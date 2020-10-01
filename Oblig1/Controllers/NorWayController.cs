aa31b3da71a8cc5af8bc6a820ebc76638e16bf75
Sasacoriror <43811709+Sasacoriror@users.noreply.github.com>
2020-10-01 17:37:50 +0200

    html endring og DAL

--- Oblig1/Controllers/NorWayController.cs
+++ Oblig1/Controllers/NorWayController.cs
@@ -6,127 +6,45 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.Extensions.Logging;
+using Oblig1.DAL;
+using Microsoft.AspNetCore.Mvc;
 
 namespace Oblig.Controllers
 {
     [Route("[controller]/[action]")]
     public class NorWayController : ControllerBase
     {
-        private readonly BillettContext _db;
+        private readonly INorwayReposatory _db;
 
-        public NorWayController(BillettContext db)
+        public NorWayController(INorwayReposatory db)
         {
             _db = db;
         }
 
-        [HttpPost]
         public async Task<bool> Lagre(NorWay BestilleBillett)
         {
-            try
-            {
-
-                var bestill = new Billett()
-                {
-                    AvgangersDato = BestilleBillett.AvgangersDato,
-                    ReturDato = BestilleBillett.ReturDato,
-                    FraSted = BestilleBillett.FraSted,
-                    TilSted = BestilleBillett.TilSted,
-                    Pris = BestilleBillett.Pris,
-                    Billettype = BestilleBillett.Billettype,
-                    Antall = BestilleBillett.Antall,
-                    Avgangstid = BestilleBillett.Avgangstid,
-                    Ankomsttid = BestilleBillett.Ankomsttid,
-                    BussNr = BestilleBillett.BussNr,
-                    AvgangstidR = BestilleBillett.AvgangstidR,
-                    AnkomsttidR = BestilleBillett.AnkomsttidR,
-                    BussNrR = BestilleBillett.BussNrR
-                };
-
-                var kunde = new Kunde()
-                {
-                    Epost = BestilleBillett.Epost
-                };
-
-                kunde.Billetter = new List<Billett>();
-                kunde.Billetter.Add(bestill);
-                _db.kunder.Add(kunde);
-                await _db.SaveChangesAsync();
-                return true;
-                
-            }
-            catch
-            {
-                return false;
-            }
+            return await _db.Lagre(BestilleBillett);
         }
 
         public async Task<List<NorWay>> HentAlle()
         {
-            
-            List<Kunde> alleKunder = await _db.kunder.ToListAsync();
-            List<NorWay> alleBilletter = new List<NorWay>();
-
-            foreach (var kunde in alleKunder)
-            {
-                foreach (var bestill in kunde.Billetter)
-                {
-                    var enBestilling = new NorWay()
-                    {
-                        Epost = kunde.Epost,
-                        AvgangersDato = bestill.AvgangersDato,
-                        ReturDato = bestill.ReturDato,
-                        FraSted = bestill.FraSted,
-                        TilSted = bestill.TilSted,
-                        Pris = bestill.Pris,
-                        Billettype = bestill.Billettype,
-                        Antall = bestill.Antall,
-                        Avgangstid = bestill.Avgangstid,
-                        Ankomsttid = bestill.Ankomsttid,
-                        BussNr = bestill.BussNr,
-                        AvgangstidR = bestill.AvgangstidR,
-                        AnkomsttidR = bestill.AnkomsttidR,
-                        BussNrR = bestill.BussNrR
-                    };
-                    alleBilletter.Add(enBestilling);
-                }
-            }
-            return alleBilletter;
+            return await _db.HentAlle();
         }
 
-
-        //Hente ut til og fra stedene
         public async Task<List<Sted>> HentStop()
         {
-            List<Sted> alleSteder = await _db.steder.ToListAsync();
-            return alleSteder;
+            return await _db.HentStop();
         }
 
         public async Task<List<PrisType>> HentPrisType()
         {
-
-            List<PrisType> allePrisType = await _db.pristype.ToListAsync();
-            return allePrisType;
+            return await _db.HentPrisType();
         }
 
-
-        //Hente ut tilpasset ruter info 
         public async Task<List<Rute>> HentRute(InfoMedRute info)
         {
-            try
-            {
-                List<Rute> alleRuter = await _db.ruter.ToListAsync();
-
-                //Finn tilpasset rute som har sammen dato og stedene ved bruk av LINQ
-                var finnRute = (from passRute in alleRuter
-                                where passRute.Dato == info.dato && passRute.FraRute == info.fSted && passRute.TilRute == info.tSted
-                                select passRute).ToList();
-
-                return finnRute;
-            }
-            catch
-            {
-                return null;
-            }
+            return await _db.HentRute(info);
         }
     }
 }
